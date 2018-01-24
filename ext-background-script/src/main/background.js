e8306b21ccc92e0737ed50f40fa09e090c3a8241
wolffdy <wolffdy0@gmail.com>
2018-01-23 20:26:59 -0500

    send webreq info to server

--- ext-background-script/src/main/background.js
+++ ext-background-script/src/main/background.js
@@ -1,1 +1,13 @@
 import request from "superagent";
+
+var filter = {urls: ["<all_urls>"]};
+var opt_extraInfoSpec = [];
+var callback = function(details) {
+    // Obviously not great to hardcode the ip and port here, but dockerizing a browser that loads an extension is nontrivial
+    request.post("http://0.0.0.0:32769")
+        .send({ method: details.method, url: details.url, timestamp: details.timeStamp})
+        .catch( (reason) => alert("request to server failed with reason: " + reason) );
+};
+
+chrome.webRequest.onBeforeRequest.addListener(
+    callback, filter, opt_extraInfoSpec);
